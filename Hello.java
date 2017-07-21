09f0bad54548f95cbfff16534502d438b2147adb
sushaw <sushaw@manh.com>
2017-07-21 17:02:31 +0530

    ah.. another syntax error correcction

--- Hello.java
+++ Hello.java
@@ -3,8 +3,9 @@ package hello;
 public class Hello{
 	public static void main(String args[]){
 		System.out.println("Printing table of 2" );
-		for(int i = 1; i < 11 ; i++);
-		System.out.println("Hello "+ i*2);
+		for(int i = 1; i < 11 ; i++){
+			System.out.println("Hello "+ i*2);
+		}
 		System.out.println("Wow just loving it");
 	}
 }
