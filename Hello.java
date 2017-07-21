2435d00b7aee8ba097eaa204527bf06f4c3dcc0f
sushaw <sushaw@manh.com>
2017-07-21 16:59:13 +0530

    corrected antoher syntax error

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ package hello;
 
 public class Hello{
 	public static void main(String args[]){
-		System.out.println("Printing table of 2" )
+		System.out.println("Printing table of 2" );
 		for(int i = 1; i < 11 ; i++);
 		System.out.println("Hello "+ i*2);
 		System.out.println("Wow just loving it");
